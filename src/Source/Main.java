5f3be012a7d75288c1bc0541905182183c1f3cac
TIMG <nhduong17ck1@gmail.com>
2021-04-24 16:00:44 +0700

    find bý slangword

---
+++ src/Source/Main.java
@@ -0,0 +1,66 @@
+package Source;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static Scanner scanner = new Scanner(System.in);
+	public static SlangWordFunction slangWordFunction = SlangWordFunction.getInstance();
+
+	public static void showMenu() {
+
+		while (true) {
+			System.out.println("\n");
+			System.out.println("=======Menu=========");
+			System.out.println("1. Search by SlangWord ");
+			System.out.println("2. Search by Definition ");
+			System.out.println("3. Show history ");
+			System.out.println("4. Add Slangword ");
+			System.out.println("5. Edit Slangword ");
+			System.out.println("6. Delete Slangword ");
+			System.out.println("7. Reset Slangword ");
+			System.out.println("8. Random Slangword ");
+			System.out.println("9. Minigame to find Definition ");
+			System.out.println("10. Minigame to find Slangword ");
+			System.out.println("11. Exit ");
+			System.out.println("YOUR CHOICE:  ");
+			String choice = scanner.nextLine();
+
+			switch (choice) {
+			case "0":
+				return;
+			case "1": {
+				cls();
+				System.out.println("Search by SlangWord");
+				String a = scanner.nextLine();
+				slangWordFunction.FindBySlangWord(a);
+				break;
+			}
+
+			case "11":
+				return;
+
+			default:
+				System.out.println("Fail! Please choose again! Enter to continue...");
+				scanner.nextLine();
+				cls();
+
+			}
+		}
+
+	}
+
+	public static void cls() {
+		try {
+			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+		} catch (Exception E) {
+			System.out.println(E);
+		}
+	}
+
+	public static void main(String[] args) {
+		showMenu();
+
+	}
+
+}
---
+++ src/Source/SlangWordFunction.java
@@ -0,0 +1,67 @@
+package Source;
+
+import java.awt.print.Printable;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import com.sun.tools.classfile.CharacterRangeTable_attribute.Entry;
+
+public class SlangWordFunction {
+	private static String SLANGWORD_URL = "src/slang_word.txt";
+	public static HashMap<String, List<String>> slangWordHashMap = new HashMap<>();
+	private static SlangWordFunction obj = new SlangWordFunction();
+
+	private SlangWordFunction() {
+		try {
+			ReadFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static SlangWordFunction getInstance() {
+		if (obj == null) {
+			synchronized (SlangWordFunction.class) {
+				if (obj == null) {
+					obj = new SlangWordFunction();// instance will be created at request time
+				}
+			}
+		}
+		return obj;
+	}
+
+	public void ReadFile() {
+		try {
+
+			File file = new File(SLANGWORD_URL);
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				if (line.contains("`")) {
+					String[] data = line.split("`");
+					String[] defintionItem = data[1].split("\\|");
+					List<String> defintions = Arrays.asList(defintionItem);
+					slangWordHashMap.put(data[0], defintions);
+				}
+			}
+			fileReader.close();
+			bufferedReader.close();
+		} catch (Exception e) {
+			System.out.println("ERROR" + e);
+		}
+	}
+
+	public void FindBySlangWord(String slangWord) {
+		List<String> results = slangWordHashMap.get(slangWord.toUpperCase());
+		System.out.println("This is results:" );
+		System.out.println(results);
+	}
+
+	
+}
